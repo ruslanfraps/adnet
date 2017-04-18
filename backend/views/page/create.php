3392c0a023fcb2e1efb71590f9e4923446993643
Ruslan <moiloginmaila@mail.ru>
2017-04-18 15:09:01 +0700

    first commit

---
+++ backend/views/page/create.php
@@ -0,0 +1,18 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $model common\models\Page */
+
+$this->title = Yii::t('backend', 'Create {modelClass}', [
+    'modelClass' => 'Page',
+]);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('backend', 'Pages'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="page-create">
+
+    <?php echo $this->render('_form', [
+        'model' => $model,
+        'upload_file' => $upload_file,
+    ]) ?>
+
+</div>
